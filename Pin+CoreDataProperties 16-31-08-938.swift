533890b27c5cb0a732effa501a6ef54468e4816a
rpaturi <rachelpaturi@gmail.com>
2016-06-02 21:03:08 -0700

    Feat: Add pin to map with city and country for the title and persist pin to the database

--- Pin+CoreDataProperties 16-31-08-938.swift
+++ Pin+CoreDataProperties 16-31-08-938.swift
@@ -2,7 +2,7 @@
 //  Pin+CoreDataProperties.swift
 //  Virtual Tourist
 //
-//  Created by Rachel Paturi on 6/1/16.
+//  Created by Rachel Paturi on 6/2/16.
 //  Copyright © 2016 Rachel Paturi. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
--- Virtual Tourist/TravelLocationVC.swift
+++ Virtual Tourist/TravelLocationVC.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import CoreData
+import ContactsUI
 
-class TravelLocationVC: UIViewController, MKMapViewDelegate {
+class TravelLocationVC: UIViewController, MKMapViewDelegate, NSFetchedResultsControllerDelegate {
+    
+    var fetchResultsController: NSFetchedResultsController!{
+        didSet{
+            fetchResultsController.delegate = self
+        }
+    }
+    
+    let appDel = UIApplication.sharedApplication().delegate as! AppDelegate
+
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -21,24 +32,62 @@ class TravelLocationVC: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         mapView.mapType = .Standard
         
-        flickrClient.searchPhotoByLocation(41.8781, longitude: 87.6298) { (result, error) in
+    }
+    
+
+    @IBAction func dropPin(sender: AnyObject) {
+        var location = ""
+        var city = ""
+        var country = ""
+        let annotation = MKPointAnnotation()
+        
+        //Get location from sender as CGPoint
+        let touchPoint = sender.locationInView(mapView)
+        
+        //Convert location to CLCoordinate2D
+        let touchCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        
+        //Create CLLocation to use for reverseGeocodeLocation
+        let touchCLLocation = CLLocation(latitude: touchCoordinate.latitude, longitude: touchCoordinate.longitude)
+        
+        //Reserve geocode location to get city and country for annotation title and add annotation to the map
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(touchCLLocation) { (placemarks, error) in
             guard (error == nil) else {
-                print("\(error)")
+                print("There was an error: \(error?.userInfo)")
                 return
             }
             
-            guard let result = result  else {
-                print("Sorry we didnt get a result")
-                return
+            var placeMark: CLPlacemark!
+            placeMark = placemarks?[0]
+            
+            // Location name
+            if let locationName = placeMark.addressDictionary!["Name"] as? NSString {
+                location = locationName as String
             }
-        }
+            
+            //City
+            if let theCity = placeMark.addressDictionary!["City"] as? NSString {
+                city = theCity as String
+            }
+            
+            //Country
+            if let theCountry = placeMark.addressDictionary!["Country"] as? NSString {
+                country = theCountry as String
+            }
+            
+            annotation.coordinate = touchCoordinate
+            
+            if location.isEmpty == false && city.isEmpty == true {
+                annotation.title = "\(location)"
+            } else {
+                annotation.title = "\(city), \(country)"
+            }
+            self.mapView.addAnnotation(annotation)
+            
+            let pin = Pin(latitude: touchCoordinate.latitude, longitude: touchCoordinate.longitude, title: annotation.title!, context: self.appDel.managedObjectContext)
+        }  
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
